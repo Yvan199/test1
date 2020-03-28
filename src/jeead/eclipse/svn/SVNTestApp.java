4651f998970f1b70cb3e7629d918b454efb377f9
yvan-navy <andryaina123@gmail.com>
2020-03-28 11:49:05 +0300

    hello from branch

--- src/jeead/eclipse/svn/SVNTestApp.java
+++ src/jeead/eclipse/svn/SVNTestApp.java
@@ -3,7 +3,7 @@ package jeead.eclipse.svn;
 public class SVNTestApp {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git");
+		System.out.println("Hello Git, from branch bug#1234 !!");
 	}
 
 }
